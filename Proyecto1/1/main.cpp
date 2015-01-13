2c330f493b6145ff1ea4d738b015e1fe66b36ad0
Luis Miranda <lcmm1993@gmail.com>
2015-01-12 21:38:05 -0430

    Inicializando los archivos del proyecto 1

---
+++ Proyecto1/1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <priority_queue>
+
+using namespace std;
+
+class arco
+{
+public:
+    int peso, ar1, ar2;
+    
+}
+
+int main( int argc, char *argv[] ){    
+    
+}
