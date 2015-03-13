72668fea9018ea2948def46e91c5ec0ee8071954
Nadie <cmedina270793@gmai.com>
2015-03-13 17:40:58 -0430

    Solucion del 1 caliche...

--- Proyecto3/p1.cpp
+++ Proyecto3/p1.cpp
@@ -1,138 +1,101 @@
-
-#include <fstream>
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
 #include <string>
-#include <string.h>
+#include <vector>
+
+#define inf 1000000000
+#define MAXN 151
 
 using namespace std;
 
-int sizeAL[150];
-int appearanceL[150][150][2];
-int sizeAcm;
-int Pos, numWord;
+int dp[MAXN][MAXN][2];
+string abbr, abbreviation;
+int abbr_len;
+string phrase;
+int phrase_len;
 
-void solve(int lastPosA, int lastWord, int lastPosWord){
-	if ((lastPosA == sizeAcm) && (lastWord == numWord)){
-		//cout << lastWord << " " << numWord << " \n";
-		++Pos;
-	}
-	else {
-		for (int i = 0; i < sizeAL[lastPosA]; i++){
+int rek(int pos = 0, int abbr_pos = 0, bool took_a_char = false) {
+    if (pos == phrase_len) return abbr_pos == abbr_len && took_a_char;
+    if (dp[pos][abbr_pos][took_a_char] != -1) return dp[pos][abbr_pos][took_a_char];
+    if (phrase[pos] == ' ') {
+        if (!took_a_char) return 0;
+        else return dp[pos][abbr_pos][took_a_char] = rek(pos+1, abbr_pos, false);
+    }
+    if (abbr_pos == abbr_len) return dp[pos][abbr_pos][took_a_char] = rek(pos+1, abbr_pos, took_a_char);
 
-			if (appearanceL[lastPosA][i][2] >= lastWord){
+    dp[pos][abbr_pos][took_a_char] = rek(pos+1, abbr_pos, took_a_char);
+    if (phrase[pos] == abbr[abbr_pos]) {
+        dp[pos][abbr_pos][took_a_char] += rek(pos+1, abbr_pos+1, true);
+    }
 
-				if ((appearanceL[lastPosA][i][2] == lastWord) && (appearanceL[lastPosA][i][1] <= lastPosWord)){
-					continue;
-				}
-				if (appearanceL[lastPosA][i][2] >(lastWord + 1)){
-					continue;
-				}
-
-				solve(lastPosA + 1, appearanceL[lastPosA][i][2], appearanceL[lastPosA][i][1]);
-			}
-		}
-	}
+    return dp[pos][abbr_pos][took_a_char];
 }
 
-int main(int argc, char *argv[]){
+int main() {
+    while (true) {
+        int t;
+        cin >> t;
+        if (!t) return 0;
 
-	int nRestrictedWords;
-	string acm, word;
-	char str[151];
-	string restrictedWords[100];
-	bool valid;
+        set<string> insignificant_words;
+        
+        string insignificant_word;
+        for (int qwertz = 0; qwertz < t; ++qwertz) {
+            cin >> insignificant_word;
+            insignificant_words.insert(insignificant_word);
+        }
+        
+        string line;
+        // read all the words
+        getline(cin, line); // read \n
 
-	// Lectura y preprocesamiento de cada grupo de casos
-	while (1){
-		scanf("%d", &nRestrictedWords);
+        while (true) {
+            getline(cin, line);
 
-		if (nRestrictedWords == 0){
-			break;
-		}
+            istringstream iss(line);
 
-		for (int i = 0; i < nRestrictedWords; i++){
+            getline(iss, abbreviation, ' ');
+            abbr = abbreviation;
+            transform(abbreviation.begin(), abbreviation.end(), abbr.begin(), ::tolower);
+            abbr_len = abbr.size();
 
-			scanf("%150s", &str);
-			restrictedWords[i] = str;
+            vector<string> words;
+            string word;
+            getline(iss, word, ' ');
+            
+            if (abbreviation == "LAST" && word == "CASE") break;
 
-		}
+            do {
+                if (!word.empty() && insignificant_words.find(word) == insignificant_words.end()) {
+                    words.push_back(word);
+                }
+            } while (getline(iss, word, ' '));
 
-		scanf("%150s", &str);
-		acm = str;
+            phrase = "";
+            for (vector<string>::iterator it = words.begin(); it != words.end(); ++it) {
+                phrase += ' ' + *it;
+            }
+            phrase_len = phrase.size();
 
-		scanf("%150s", &str);
-		word = str;
+            memset(dp, -1, sizeof dp);
+            int sol = rek(1);
+            
+            if (sol) cout << abbreviation << " can be formed in " << sol << " ways" << endl;
+            else cout << abbreviation << " is not a valid abbreviation" << endl;
+        }
+    }
 
-		sizeAcm = acm.size();
-		numWord = 0;
-
-		memset(sizeAL, 0, sizeof(int) * 150);
-
-		// Lectura y pre procesamiento de cada caso
-		while (islower(word[0])){
-
-			valid = true;
-
-			for (int i = 0; i < nRestrictedWords; i++){
-				if (!restrictedWords[i].compare(word)){
-					valid = false;
-					break;
-				}
-			}
-
-			if (valid){
-				for (int i = 0; i < word.size(); i++){
-					for (int j = 0; j < sizeAcm; j++){
-						if (tolower(acm[j]) == word[i]){
-							if (j == 0 && numWord != 0){
-								continue;
-							}
-							appearanceL[j][sizeAL[j]][1] = i;
-							appearanceL[j][sizeAL[j]][2] = numWord;
-							++sizeAL[j];
-
-						}
-					}
-				}
-			}
-			scanf("%150s", &str);
-			word = str;
-
-
-			if (isupper(word[0])){
-
-				Pos = 0;
-				for (int k = 0; k < sizeAL[0]; k++){
-					solve(1, 0, appearanceL[0][k][1]);
-				}
-
-				if (Pos == 0){
-					printf("%s is not a valid abbreviation\n", acm.c_str());
-				}
-				else {
-					printf("%s can be formed in %i ways\n", acm.c_str(), Pos);
-				}
-
-				acm = word;
-
-				numWord = 0;
-				memset(sizeAL, 0, sizeof(int) * 150);
-				sizeAcm = acm.size();
-				scanf("%150s", &str);
-				word = str;
-				valid = false;
-			}
-
-			if (valid){
-				++numWord;
-			}
-
-		}
-
-	}
-	//while (1){
-
-	//};
+    return 0;
 }
